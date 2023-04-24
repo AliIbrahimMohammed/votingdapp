706a56fc23111b4ea7d712b88fcac1959130d0d7
Ali Ibrahim Mohammed <104573166+AliIbrahimMohammed@users.noreply.github.com>
2023-04-24 17:26:47 +0200

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,27 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const address = useAddress();
-  const contractAddress = "0x3dE6c33921f117906c70FEd03DA6852b9F46F700";
-
+  const contractAddress = "0xBDEf739Ca2B9B9747D130D4A4F9FF0CA68b4bd38";
+  
   const { contract } = useContract(contractAddress);
   const { data: proposal, isLoading: proposalLoading } = useContractRead(contract,"proposals",0);
   const { data: hasVoted, isLoading: hasVotedLoading } = useContractRead(contract,"hasVoted",0,address);
   const { data: proposal1, isLoading: proposalLoading1 } = useContractRead(contract,"proposals",1);
   const { data: hasVoted1, isLoading: hasVotedLoading1 } = useContractRead(contract,"hasVoted",1,address);
-
-
+  const { data: cannotVote, isLoading: cannotVoteLoading } = useContractRead(contract,"cannotVote",0,address);
+  const { data: cannotVote1, isLoading: cannotVoteLoading1 } = useContractRead(contract,"cannotVote",1,address);
+  const { data: canVote1, isLoading: canVoteLoading1 } = useContractRead(contract,"canVote",1,address);
+  const { data: canVote, isLoading: canVoteLoading } = useContractRead(contract,"canVote",0,address);
 
   return (
     <div className={styles.container}>
+      {/* <h1>Voting Dapp System</h1> */}
       <main className={styles.main}>
+        <h1>Voting Dapp System</h1>
+        <p>This is a voting system based on the Ethereum Blockchain technology and uploaded to the Polygon Mumbai network.</p>
           <ConnectWallet/>
-        <h1> Voting Dapp </h1>
         <div className={styles.card}>
-          <h1>The first question</h1>
+          {/* <h2> : : </h2> */}
           <div>
             {address ? (
               <div>
@@ -36,20 +40,24 @@ const Home: NextPage = () => {
                         contractAddress={contractAddress}
                         action={(contract) => contract.call("vote",0,true)}
                         isDisabled={hasVoted}
+                        onSuccess={(result) => alert("You voted successfully")}
+                        onError={(error) => alert("You do not have the authority to vote.")}
                       >Yes</Web3Button>
                       <br/>
                       <Web3Button
                         contractAddress={contractAddress}
                         action={(contract) => contract.call("vote",0,false)}
                         isDisabled={hasVoted}
+                        onSuccess={(result) => alert("You voted successfully")}
+                        onError={(error) => alert("You do not have the authority to vote.")}
                       >No</Web3Button>
                     </div>
                     <div>
                       {!hasVotedLoading && hasVoted ? (
                         <div>
                           <h3>Results: </h3>
-                          <p>Yes: {proposal[1].toNumber()}</p>
-                          <p>No: {proposal[2].toNumber()}</p>
+                          <p>Yes : {proposal[1].toNumber()}</p>
+                          <p>No : {proposal[2].toNumber()}</p>
                         </div>
                       ) : (
                         <div>
@@ -67,9 +75,9 @@ const Home: NextPage = () => {
             )}
           </div>
         </div>
-        {/* ===================================== */}
+        {/* ======================================================================================== */}
         <div className={styles.card}>
-          <h1>second question</h1>
+          {/* <h2> : : </h2> */}
           <div>
             {address ? (
               <div>
@@ -85,20 +93,24 @@ const Home: NextPage = () => {
                         contractAddress={contractAddress}
                         action={(contract) => contract.call("vote",1,true)}
                         isDisabled={hasVoted1}
+                        onSuccess={(result) => alert("You voted successfully")}
+                        onError={(error) => alert("You do not have the authority to vote.")}
                       >Yes</Web3Button>
                       <br/>
                       <Web3Button
                         contractAddress={contractAddress}
                         action={(contract) => contract.call("vote",1,false)}
                         isDisabled={hasVoted1}
+                        onSuccess={(result) => alert("You voted successfully")}
+                        onError={(error) => alert("You do not have the authority to vote.")}
                       >No</Web3Button>
                     </div>
                     <div>
                       {!hasVotedLoading1 && hasVoted1 ? (
                         <div>
                           <h3>Results: </h3>
-                          <p>Yes: {proposal1[1].toNumber()}</p>
-                          <p>No: {proposal1[2].toNumber()}</p>
+                          <p>Yes : {proposal1[1].toNumber()}</p>
+                          <p>No : {proposal1[2].toNumber()}</p>
                         </div>
                       ) : (
                         <div>
